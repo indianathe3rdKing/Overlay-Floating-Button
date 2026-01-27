525d9bd5237417cd9dc8ee7cf64590b57ef63a51
indianathe3rd <indiemenziwa@gmail.com>
2026-01-27 15:02:54 +0200

    Path.moveTo implement

--- app/src/main/java/com/example/overlayfloatingbutton/gesture/Gesture.kt
+++ app/src/main/java/com/example/overlayfloatingbutton/gesture/Gesture.kt
@@ -7,4 +7,12 @@ import android.graphics.Point
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.random.Random
+import kotlin.random.nextInt
 
+fun Path.moveTo(position: Point,random: Random){
+    if(random== null) safeMoveTo(postion.x, position.y)
+    else safeMoveTo(
+        random.nextIntInOffset(position.x, RANDOMIZATION_POSITION_MAX_OFFSET_PX),
+        random.nextIntInOffset(position.y, RANDOMIZATION_POSITION_MAX_OFFSET_PX),
+    )
+}
