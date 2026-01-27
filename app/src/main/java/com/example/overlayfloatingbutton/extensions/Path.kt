3a46079f1a924f34070f687c9de4188017313838
indianathe3rd <indiemenziwa@gmail.com>
2026-01-27 15:35:28 +0200

    Add safeLineTo

--- app/src/main/java/com/example/overlayfloatingbutton/extensions/Path.kt
+++ app/src/main/java/com/example/overlayfloatingbutton/extensions/Path.kt
@@ -6,3 +6,5 @@ import kotlin.math.max
 fun Path.safeMoveTo(x: Int,y: Int): Unit=
     moveTo(max(0,x).toFloat(),max(0,y).toFloat())
 
+fun Path.safeLineTo(x: Int,y: Int): Unit=
+    lineTo(max(0,x).toFloat(),max(0,y).toFloat())
